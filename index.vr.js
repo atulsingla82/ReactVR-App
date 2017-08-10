d370eb68b6284682f1ade4960b03d05ed0007b1b
atulsingla82 <asingla@wgu.edu>
2017-08-10 10:15:56 -0400

    Testing props

--- index.vr.js
+++ index.vr.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {
   AppRegistry,
   asset,
   Pano,
   Text,
-  View,
+  View
 } from 'react-vr';
 
-export default class ReactVR extends React.Component {
+class Task extends Component {
+      render(){
+      return(
+       <Text style={{textAlign:'center', fontSize: 0.2 }}> Todo: {this.props.text} </Text>
+
+        )
+    }
+}
+
+export default class ReactVR extends Component {
   render() {
     return (
       <View>
         <Pano source={asset('starry-sky.jpg')}/>
-        
+         <View
+           style= {{
+           transform:[{translate:[0,0,-3]}],
+           layoutOrigin:[0.5,0.5] 
+         }}>
+
+           <Task text='Get groceries'/>
+           <Task text='Finish homework'/>
+           <Task text='Do laundry'/>
+         </View>
+  
       </View>
     );
   }
---
+++ sections/pano/index.vr.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  Pano,
+  Text,
+  View,
+} from 'react-vr';
+
+export default class ReactVR extends React.Component {
+  render() {
+    return (
+      <View>
+        <Pano source={asset('starry-sky.jpg')}/>
+        
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('ReactVR', () => ReactVR);
