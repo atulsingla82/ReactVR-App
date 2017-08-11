13879f0f1c1fdf2cf5e671b014241318aaa21fca
atulsingla82 <asingla@wgu.edu>
2017-08-11 19:37:16 -0400

    ✌️

--- index.vr.js
+++ index.vr.js
@@ -4,38 +4,58 @@ import {
   asset,
   Pano,
   Text,
-  View
+  View,
+  StyleSheet
 } from 'react-vr';
 
-class Task extends Component {
-      render(){
-      return(
-       <Text style={{textAlign:'center', fontSize: 0.2 }}> Todo: {this.props.text} </Text>
+class Row extends Component {
 
-        )
+    render () {
+     return (
+     
+    <View style = {[{ backgroundColor:this.props.color}, styles.row]} >
+     <Text style={styles.text}>{this.props.color}</Text>
+     </View>
+      )
     }
 }
 
 export default class ReactVR extends Component {
-  render() {
-    return (
-      <View>
-        <Pano source={asset('starry-sky.jpg')}/>
-         <View
-           style= {{
-           transform:[{translate:[0,0,-3]}],
-           layoutOrigin:[0.5,0.5] 
-         }}>
-
-           <Task text='Get groceries'/>
-           <Task text='Finish homework'/>
-           <Task text='Do laundry'/>
-         </View>
   
+  render() {
+
+    
+    return (
+      <View style= {styles.container}>
+        <Pano source={asset('starry-sky.jpg')}/>
+        <Row color = 'red'/>
+         <Row color = 'blue'/>
+          <Row color = 'green'/>
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  text:{
+    fontSize: 0.2,
+    textAlign:'center'
+
+  },
+  row:{
+   width :0.5,
+   height:0.3,
+   margin:0.1
+  },
+  container :{
+
+    flex:1,
+    width:2,
+    flexDirection:"row",
+    transform:[{translate:[-1,0,-3]}]
+  }
+
+})
 
 AppRegistry.registerComponent('ReactVR', () => ReactVR);
 
--- sections/pano/index.vr.js
+++ sections/pano/index.vr.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {
   AppRegistry,
   asset,
   Pano,
   Text,
-  View,
+  View
 } from 'react-vr';
 
-export default class ReactVR extends React.Component {
+
+
+export default class ReactVR extends Component {
+  
+  constructor() {
+   super();
+
+   this.state = {
+
+     showSign:true
+   };
+
+   setInterval (() => this.setState ({showSign: !this.state.showSign}), 1000);
+
+  }
+
+
   render() {
+
+    let message = this.state.showSign === true ? "Welcome to the VR land" : " "
     return (
       <View>
         <Pano source={asset('starry-sky.jpg')}/>
-        
+         <Text style = {{fontSize: 0.2 , transform:[{translate:[-1,0,-2]}]}}> {message}
+          
+         </Text>
+  
       </View>
     );
   }
