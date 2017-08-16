a0304b63f6330e344fd0d122e26f0c88dfd57de8
atulsingla82 <asingla@wgu.edu>
2017-08-15 23:28:51 -0400

    Practicing event handlers

--- index.vr.js
+++ index.vr.js
@@ -9,70 +9,40 @@ import {
   Box
 } from 'react-vr';
 
-class Row extends Component {
 
-    render () {
-     return (
-     
-    <View style = {[{ backgroundColor:this.props.color}, styles.row]} >
-    
-     </View>
-      )
-    }
-}
 
 export default class ReactVR extends Component {
   
+
+  constructor (){
+    super();
+    this.state = {
+
+    fontSize:0.1
+    }
+  }
   render() {
 
     
     return (
-      <View style= {styles.container}>
+      <View >
         <Pano source={asset('starry-sky.jpg')}/>
-        <Box 
-    dimwidth={0.5}
-    dimHeight={0.5}
-    dimDepth= {0.5}
-    wireFrame={true}
-    style={{
-    color:'red',
-    transform:[
-    {translate:[0,0,-3]},
-    {translateY:1},
-    {translateX:-0.5},
-    {rotateY:45},
-    {rotateZ:45}
+        <Text
+        onEnter={()=> this.setState({fontSize:0.5})}
+        onExit= {()=> this.setState({fontSize:0.1})}
+      style ={{
+      fontSize:this.state.fontSize,
+      transform:[{translate:[0,0,-2]}]
 
-    ]
-
-
-    }}
-    />
+      }}>
+         HOVER OVER ME!!!
+        </Text>
       </View>
     );
   }
 };
 
-const styles = StyleSheet.create({
-  text:{
-    fontSize: 0.2,
-    textAlign:'center'
 
-  },
-  row:{
-   width :0.5,
-   height:0.3,
-   margin:0.1
-  },
-  container :{
-
-    flex:1,
-    width:2,
-    flexDirection:"row",
-    transform:[{translate:[-1,0,-3]}]
-  }
-
-})
 
 AppRegistry.registerComponent('ReactVR', () => ReactVR);
 
